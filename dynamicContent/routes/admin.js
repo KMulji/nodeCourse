cce5323c153b5a83c0298b3c17e1591bf7ff6068
KMulji <muljikyan@gmail.com>
2024-04-16 17:28:45 +0300

    pug engine

--- dynamicContent/routes/admin.js
+++ dynamicContent/routes/admin.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const products = [];
+
+//display products page
 router.get('/add-product', (req, res, next) => {
     console.log('Product Page', req.url);
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.render('add-product',{pageTitle:"Add Product"})
 });
+
+//post data to form
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     products.push({ title: req.body.title });
