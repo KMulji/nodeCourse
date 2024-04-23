f6a6a8530d0ff99afd64d9d519d9c4e256249d02
Kyan Mulji <muljikyan@gmail.com>
2024-04-23 15:09:43 +0300

    dynamic edit

--- DynamicRoutes/controllers/shop.js
+++ DynamicRoutes/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -9,12 +10,12 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
-exports.getProduct = (req,res,next)=>{
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId,product=>{
-    res.render('shop/product-detail',{pageTitle:product.title,product:product,path:'/products'});
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', { pageTitle: product.title, product: product, path: '/products' });
   })
-  
+
 };
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -32,6 +33,15 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  prodId = req.body.productId;
+  Product.findById(prodId,(product)=>{
+    Cart.addProduct(prodId,product.price);
+  });
+  
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
